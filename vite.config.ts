0dc00fb3674bf04e143e42829873b35cd55857eb
iosifidis <eiosifidis@gmail.com>
2026-03-16 22:58:39 +0200

    changed vite file

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
-    base: '/mathe-ellak/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
